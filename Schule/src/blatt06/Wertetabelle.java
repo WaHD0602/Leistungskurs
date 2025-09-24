f7f80f8df866434da95b379ae55d89d80cdb0236
WaHD0602 <noah.voegele@icloud.com>
2025-09-24 11:20:17 +0200

    02.09 Unterricht

---
+++ Schule/src/blatt06/Wertetabelle.java
@@ -0,0 +1,19 @@
+package blatt06;
+import java.util.Scanner;
+public class Wertetabelle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //baba ist besserer name
+        int y = 0;
+        int x = -2;
+        int zwischenergebnis = 0;
+
+        int grad = sc.nextInt();
+
+        double[] coeff = new  double[]{grad + 1};
+        for (int i = 0; i < coeff.length; i++) {
+            coeff[i] = sc.nextDouble();
+        }
+
+
+    }
+}
