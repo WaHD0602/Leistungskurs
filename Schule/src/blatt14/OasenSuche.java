e00fc32c840abd48ccef79402a869338acebc816
WaHD0602 <noah.voegele@icloud.com>
2026-01-14 11:17:45 +0100

    02.09 Unterricht

--- Schule/src/blatt14/OasenSuche.java
+++ Schule/src/blatt14/OasenSuche.java
@@ -5,6 +5,7 @@ import schisch_visualizer.*;
 public class OasenSuche {
 
     static char[][] spielfeld;
+    static SchischVisualizer sv;
 
     public static void initialisiereSpielfeld(int l, int b) {
         spielfeld = new char[l][b];
@@ -27,6 +28,10 @@ public class OasenSuche {
         }
     }
 
+    public static void energy(int ws) {
+        int energy = ws;
+    }
+
 
     public static int[] findeSpieler() {
         for (int i = 0; i < spielfeld.length; i++) {
@@ -39,24 +44,102 @@ public class OasenSuche {
         return null;
     }
 
-    public static void findeWasser() {
-        //zufallswert von 1-4
-        int zufall = blatt13.Zufall.zufallGanz(1, 5);
-        if (zufall == 1) {
+    public static void findeWasser(int energy) {
 
+
+
+        while (true) {
+            //zufallswert von 1-4
+            sv.step(spielfeld);
+            int zufall = blatt13.Zufall.zufallGanz(1, 5);
+            int[] spielerposition = findeSpieler();
+            if (zufall == 1 && spielerposition[1] > 0) {
+                if (spielfeld[spielerposition[0]][spielerposition[1] - 1] != 'A') {
+                    spielfeld[spielerposition[0]][spielerposition[1]] = '4';
+                    spielfeld[spielerposition[0]][spielerposition[1] - 1] = 'P';
+                    energy--;
+
+                }
+            }
+            if (zufall == 2 && spielerposition[1] < spielfeld[0].length-1) {
+
+                if (spielfeld[spielerposition[0]][spielerposition[1] + 1] != 'A') {
+                    spielfeld[spielerposition[0]][spielerposition[1]] = '4';
+                    spielfeld[spielerposition[0]][spielerposition[1] + 1] = 'P';
+                    energy--;
+                }
+            }
+            if (zufall == 3 && spielerposition[0] > 0) {
+
+                if (spielfeld[spielerposition[0] - 1][spielerposition[1]] != 'A') {
+                    spielfeld[spielerposition[0]][spielerposition[1]] = '4';
+                    spielfeld[spielerposition[0] - 1][spielerposition[1]] = 'P';
+                    energy--;
+                }
+            }
+            if (zufall == 4 && spielerposition[0] < spielfeld.length-1) {
+
+                if (spielfeld[spielerposition[0] + 1][spielerposition[1]] != 'A') {
+                    spielfeld[spielerposition[0]][spielerposition[1]] = '4';
+                    spielfeld[spielerposition[0] + 1][spielerposition[1]] = 'P';
+                    energy--;
+                }
+            }
+
+
+            spielerposition = findeSpieler();
+
+            if (spielerposition[0] > 0) {
+                if (spielfeld[spielerposition[0]-1][spielerposition[1]] == '6') {
+                    System.out.println("Sie haben Wasser gefunden!");
+                    break;
+                }
+            }
+
+
+            if (spielerposition[0] < spielfeld.length - 1) {
+                if (spielfeld[spielerposition[0]+1][spielerposition[1]] == '6') {
+                    System.out.println("Sie haben Wasser gefunden!");
+                    break;
+                }
+            }
+
+
+            if (spielerposition[1] > 0) {
+                if (spielfeld[spielerposition[0]][spielerposition[1]-1] == '6') {
+                    System.out.println("Sie haben Wasser gefunden!");
+                    break;
+                }
+            }
+
+            if (spielerposition[1] < spielfeld[0].length - 1) {
+                if (spielfeld[spielerposition[0]][spielerposition[1]+1] == '6') {
+                    System.out.println("Sie haben Wasser gefunden!");
+                    break;
+                }
+            }
+
+
+            if (energy <= 0) {
+                System.out.println("Sie sind verdurstet!");
+                break;
+            }
         }
     }
 
 
 
+
     public static void main(String[] args) {
-        SchischVisualizer sv = new SchischVisualizer();
+        sv = new SchischVisualizer();
         initialisiereSpielfeld(60,60);
         zufallsPositionSpieler();
         sv.step(spielfeld);
-        wasserZufall(0.03);
+        wasserZufall(0.0003);
         sv.step(spielfeld);
         steinZufall(0.15);
+        sv.step(spielfeld);
+        findeWasser(5000);
         sv.step(spielfeld);
         sv.start();
     }
