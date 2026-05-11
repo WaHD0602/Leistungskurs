817fc792417ad99b9e8e5c255884025f1df44c3b
WaHD0602 <noah.voegele@icloud.com>
2026-05-11 09:29:12 +0200

    02.09 Unterricht

---
+++ Schule/src/blatt29_aufgabe2/Node.java
@@ -0,0 +1,22 @@
+package blatt29_aufgabe2;
+
+import java.util.LinkedList;
+
+public class Node {
+
+    int value;
+    Node next;
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    public Node getNext() {
+        return this.next;
+    }
+
+
+}
